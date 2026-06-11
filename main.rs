efdea075f0530fe8904c9ace79c30cc641a484ce
Gilvanir Sousa <gilvanir87@gmail.com>
2026-06-11 16:02:08 -0300

    primeiro commit

---
+++ main.rs
@@ -0,0 +1,23 @@
+use std::io;
+
+fn main() {
+    let mut numero1 = String::new();
+    let mut numero2 = String::new();
+
+    println!("Digite o primeiro número:");
+    io::stdin()
+        .read_line(&mut numero1)
+        .expect("Erro ao ler o número");
+
+    println!("Digite o segundo número:");
+    io::stdin()
+        .read_line(&mut numero2)
+        .expect("Erro ao ler o número");
+
+    let n1: f64 = numero1.trim().parse().expect("Número inválido");
+    let n2: f64 = numero2.trim().parse().expect("Número inválido");
+
+    let resultado = n1 + n2;
+
+    println!("Resultado: {}", resultado);
+}
